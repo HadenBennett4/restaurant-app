34030c759648826ee984d03b0d732d19bf2241a6
mario0220 <80548453+mario0220@users.noreply.github.com>
2021-04-07 17:17:28 -0500

    removed free dessert code

--- src/components/totalBox.vue
+++ src/components/totalBox.vue
@@ -28,7 +28,6 @@
                 Total - ${{computeTotal().toFixed(2)}}
                 <br>
                 <br>
-                <!--{{Discount()}}-->
                 <router-link align="right" :to="{ path: '/payment' }" class="button3">
                     <font size="+3">Click to Pay</font>
                 </router-link>
@@ -44,11 +43,6 @@
 <script>
 export default {
   methods: {
-      Discount(){
-        if(Math.floor(Math.random() * 3) + 1 == 2){
-          alert("Congratulations you have won a free dessert!!");
-        }
-     },
       computeTotal(){
         var i;
         var tot = 0;
